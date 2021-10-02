eb0d30518c91200662d96205982cf6d63a9ebd76
Israel Veras <israelveras@gmail.com>
2021-10-02 18:18:35 -0300

    Class Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,16 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
 		
 		System.out.println("Chess!!!");
 
+		Position position = new Position(1, 3);
+		System.out.println(position);
+		
 	}
 
 }
